629d91bc5fa84c9771cc322cb8c6d1beb2b627cf
Jason Slavin [C] <jslavin@marketo.com>
2017-07-02 14:05:47 -0700

    Mid-setting up gulp build. Need to create gulpfile JS.

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-<?php
 <html>
 <head>
 </head>
 <body>
-    <h1> Hello Wolrd </h1>
+    <article class="welcome">
+        <div class="hello-world">
+            <h1> Hello Wolrd </h1>
+            <h2>I am writing HTML.</h2>
+            <?php
+            $myFirstArray = array(
+                "name"  => "Jason",
+                "age"   => "32",
+                "pizza" => "yes"
+            )
+            ?>
+            <p> Here is a p-tage, with my name from a php array in it! <?php echo $myFirstArray["name"]?></p>
+        </div>
+    </article>
 </body>
 </html>
-?>
